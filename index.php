455ccef7f4a33032f1d0e1ce4a77b6894669a075
AndreSantos03 <93008364+AndreSantos03@users.noreply.github.com>
2023-04-28 15:29:33 +0100

    minor

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-$password = "password";
-$hash = password_hash($password, PASSWORD_DEFAULT);
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="styles.css">
+    <body>
+        <header>
+            <h1>Sexo!</h1>
+        </header>
+    </body>
