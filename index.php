5a12f92874f35cb63a2e4366aa3083b1bddb185b
Luís Contreiras <luis.m.contreiras@gmail.com>
2023-04-29 19:53:11 +0100

    Login Form

--- index.php
+++ index.php
@@ -10,11 +10,16 @@
         <div>
             <img class="logo" src="assets/logo.png" alt="logo">
         </div>
-        <div>
-            <img class="image" src="assets/homepage.png" alt="illustration">
-        </div>
-        <div class="retangulo">
-
-        </div>
     </header>
+    <div>
+        <img class="image" src="assets/homepage.png" alt="illustration">
+    </div>
+    <div class="retangulo">
+    </div>
+    <div class="login_form_box">
+        <div class="login_members_form_box">
+            <h3 id="login_members">ALREADY MEMBERS</h3>
+            <button id="login_btnHelp">Need help?</button>
+        </div>
+    </div>
 </body>
