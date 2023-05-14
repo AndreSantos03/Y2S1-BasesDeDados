9b344cd0fde312e6efd44b1511ae160529121357
Luís Contreiras <luis.m.contreiras@gmail.com>
2023-05-14 15:46:44 +0100

    Added simple templates

--- pages/index.php
+++ pages/index.php
@@ -4,8 +4,23 @@ declare(strict_types=1);
 require_once(__DIR__ . '/../utils/session.php');
 $session = new Session();
 
-if ($session->isLoggedIn())
+if ($session->isLoggedIn()) {
     header('Location: ../pages/main.php');
-else
-    header('Location: ../pages/login.php');
+}
+require_once(__DIR__ . '/../templates/common.tpl.php');
+require_once(__DIR__ . '/../templates/user.tpl.php');
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Helpline</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="/../css/styles.css">
+</head>
+
+<body>
+    <?php drawIndex(); ?>
+    <?php drawLoginForm(); ?>
+</body>
---
+++ templates/common.tpl.php
@@ -0,0 +1,12 @@
+<?php function drawIndex() { ?>
+<header>
+    <div>
+        <img class="logo" src="/../assets/logo.png" alt="logo">
+    </div>
+</header>
+<div>
+    <img class="image" src="/../assets/homepage.png" alt="illustration">
+</div>
+<div class="retangulo">
+</div>
+<?php } ?>
