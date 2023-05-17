ba8d15df0c8e2cc32a68b81157b5ae5bfb3f4817
Luís Contreiras <luis.m.contreiras@gmail.com>
2023-05-17 10:34:21 +0100

    Ticket pagination

--- pages/main.php
+++ pages/main.php
@@ -74,27 +74,11 @@ if (!$session->isLoggedIn()) {
                 </svg>
             </button>
         </div>
-        <div class="ticket">
-            <div class="ticket_status_spacer">
-                <div class="ticket_status">
-                    <p>Closed</p>
-                </div>
-            </div>
-            <div class="ticket_content">
-                <div class="ticket_title">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                        <path fill-rule="evenodd"
-                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    <p>What’s the difference between String and string in C#?</p>
-                </div>
-                <div class="ticket_desc">
-                    <p>string s = "Hello world!"; String s = "Hello world!"; …</p>
-                </div>
-            </div>
-            <div class="ticket_info">
-                <p>Asked by Lance Fisher 12 jan 12:52</p>
-            </div>
+        <div class="ticket_list" id="ticket_list">
         </div>
+    </div>
+    <div class="pagenumbers" id="pagination">
+
+    </div>
+    <script src="../js/main.js"></script>
 </body>
