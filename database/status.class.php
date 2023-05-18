be8637af728ef5760cdcbcf715880a51cc951cb2
Luís Contreiras <luis.m.contreiras@gmail.com>
2023-05-18 11:57:59 +0100

    Status class done

--- database/status.class.php
+++ database/status.class.php
@@ -1,25 +1,43 @@
 <?php
-class Status{
-    public int $id;
-    public int $agent_id;
-    public int $admin_id;
-    public string $status;
-    public string $priority;
-    public string $datetime;
+class Status {
+    private int $id;
+    private int $ticketId;
+    private int $agentId;
+    private ?int $adminId;
+    private string $status;
+    private string $priority;
+    private string $datetime;
 
-public function __construct(int $id, int $agent_id,int $admin_id, string $status,string $priority, string $datetime ){
-    $this->id = $id;
-    $this->agent_id = $agent_id;
-    $this->admin_id = $admin_id;
-    $this->status = $status;
-    $this->priority = $priority;
-    $this->datetime = $datetime;
+    public function __construct(
+        int $ticketId, 
+        int $agentId, 
+        ?int $adminId, 
+        string $status, 
+        string $priority, 
+        string $datetime
+        ) {
+        $this->ticketId = $ticketId;
+        $this->agentId = $agentId;
+        $this->adminId = $adminId;
+        $this->status = $status;
+        $this->priority = $priority;
+        $this->datetime = $datetime;
+    }
+    function save($db) {
+        $stmt = $db->prepare('
+            INSERT INTO Status (ticket_id, agent_id, admin_id, status, priority, datetime)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ');
+        
+        $stmt->execute(array($this->ticketId, $this->agentId, $this->adminId, $this->status, $this->priority, $this->datetime));
+    }
+    static function getStatusByTicketId($db, $ticketId) {
+        $stmt = $db->prepare('
+            SELECT * FROM Status WHERE ticket_id = ? ORDER BY datetime DESC LIMIT 1
+        ');
+        $stmt->execute(array($ticketId));
+        $status = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $status;
+    }    
 }
-
-
-
-}
-
-
-
 ?>
